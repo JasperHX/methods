f9b2d8b40135a9cd7603b671d1510e6a335fe4c0
JasperHX <jasper-hendrikx@hotmail.com>
2021-09-02 17:05:50 +0200

    huiswerk

--- string-methods.js
+++ string-methods.js
@@ -13,7 +13,7 @@ console.log(word.length);
 const fruit = "Banaan";
 const vegetable = "Aardappel";
 
-
+console.log(fruit.length > vegetable.length);
 
 /* Opdracht 2 */
 //  Zorg ervoor dat de laatste letter van het woord in fruit en vegetable (hierboven) in de terminal gelogd wordt.
@@ -21,15 +21,22 @@ const vegetable = "Aardappel";
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
 
+const lastIndexFruit = fruit.length -1;
+const lastIndexVegetable = vegetable.length -1;
 
+const lastFruit = fruit.substring(lastIndexFruit)
+const lastVegetable = vegetable.substring(lastIndexVegetable);
+
+console.log(lastFruit, lastVegetable);
 
 /* Opdracht 3 */
 //  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
 // ---- Verwachte uitkomst: "De kat krabt de krullen van de trap"
 
 const userInput = "  De kat krabt de krullen van de trap ";
+const trimmed = userInput.trim();
 
-
+console.log(trimmed);
 
 /* Opdracht 4 */
 // Check of het woord "Fantastisch" in onderstaande zin voorkomt en log dit in de terminal.
@@ -37,13 +44,18 @@ const userInput = "  De kat krabt de krullen van de trap ";
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
 
+const checked = story.includes("Fantastisch")
 
+console.log(checked);
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
 
+const shorter = story.indexOf(".")
+const sentence = story.substring(0, shorter + 1);
 
+console.log(sentence);
 
 /* Opdracht 6 */
 // Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
@@ -53,6 +65,10 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 // ---- Verwachte uitkomst: "€35,59"
 
 const price = "$35.59";
+
+const newPrice = price.replace("$","€").replace(".",",");
+
+console.log(newPrice);
 
 
 
